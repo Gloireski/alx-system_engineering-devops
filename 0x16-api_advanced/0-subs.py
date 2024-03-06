90a97a7ebc8b6789954fa9c6b5b2561ade86a820
Gloireski <belemgloirebekoutou8@gmail.com>
2024-03-06 01:43:21 +0000

    task 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,11 @@ def number_of_subscribers(subreddit):
 
     sub_inf = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit),
-                           headers={"User-Agent": "My-User-Agent"})
+                           headers={"User-Agent": "Custom"},
+                           allow_redirects=False)
 
     if sub_inf.status_code == 200:
         sub_inf.json().get("data").get("subscribers")
     else:
+        #print(sub_inf.status_code)
         return 0
