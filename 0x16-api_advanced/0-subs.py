eccecf67830759e085a0535c113ec966401988ea
Gloireski <belemgloirebekoutou8@gmail.com>
2024-03-06 01:09:28 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,14 @@
 
 def number_of_subscribers(subreddit):
     """Queries Reddit APi and return of suscribers"""
-    import requets
+    import requests
 
     sub_inf = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit),
                            headers={"User-Agent": "My-User-Agent"},
                            allow_redirects=False)
 
-    if sub_info.status_code >= 300:
+    if sub_inf.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    return sub_inf.json().get("data").get("subscribers")
