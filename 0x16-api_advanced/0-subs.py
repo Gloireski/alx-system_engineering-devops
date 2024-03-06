89c571cce5c0b983ad971ef00abddb1a6e499f5f
Gloireski <belemgloirebekoutou8@gmail.com>
2024-03-06 01:04:39 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+
+def number_of_subscribers(subreddit):
+    """Queries Reddit APi and return of suscribers"""
+    import requets
+
+    sub_inf = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit),
+                           headers={"User-Agent": "My-User-Agent"},
+                           allow_redirects=False)
+
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
